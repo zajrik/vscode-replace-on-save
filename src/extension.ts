a6911bd4ee18f105dcf8eef9431763d128f466c0
Zack Campbell <zajrik@users.noreply.github.com>
2026-01-22 15:07:27 -0600

    add esbuild to bundle minimatch dependency
    also removed the test that wasn't doing anything and was failing after the removal of glob

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import * as minimatch from 'minimatch';
+import { minimatch } from 'minimatch';
 
 interface Rule {
   search: string;
